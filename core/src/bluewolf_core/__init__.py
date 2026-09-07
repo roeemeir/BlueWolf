22318a0480cc90f1b4bba673c61ff4f8bc61db03
roeemeir <rughroee2000@gmail.com>
2026-09-07 09:23:30 +0300

    core v2.5: expose latest-cycle CoreSession

--- core/src/bluewolf_core/__init__.py
+++ core/src/bluewolf_core/__init__.py
@@ -38,7 +38,7 @@ from .models import (
 from .route_detection import RouteDetection, detect_closed_route
 from .scoring import aggregate_group_scores, score_error, score_vehicle
 from .session import CheckpointCompatibilityError
-from .session_v23 import CoreSession
+from .session_v25 import CoreSession
 from .templates import (
     MemberTemplateFit,
     NoLegalTemplateAssignment,
