354127e57d597284bb3a6e9fcc315aab1f0ccf45
roeemeir <rughroee2000@gmail.com>
2026-09-07 09:00:59 +0300

    core service: activate v2.4 worker

--- core/service/http_service.py
+++ core/service/http_service.py
@@ -13,7 +13,7 @@ from http import HTTPStatus
 from http.server import BaseHTTPRequestHandler, ThreadingHTTPServer
 from typing import Any, Mapping
 
-from bluewolf_core.worker_v23 import CoreWorker
+from bluewolf_core.worker_v24 import CoreWorker
 
 
 class Handler(BaseHTTPRequestHandler):
