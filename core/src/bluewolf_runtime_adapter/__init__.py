a156555f0e856b8260680ebc0bf84173f057b8f1
roeemeir <rughroee2000@gmail.com>
2026-09-09 18:51:42 +0300

    Export transactional live ingest coordinator

--- core/src/bluewolf_runtime_adapter/__init__.py
+++ core/src/bluewolf_runtime_adapter/__init__.py
@@ -1,11 +1,12 @@
 """Application-facing adapters around the algorithmic Blue Wolf core.
 
-This package may know about transport/presentation contracts.  The public
+This package may know about transport/presentation contracts. The public
 ``bluewolf_core`` package remains independent of HTTP, UI colors, arenas and
 other application concerns.
 """
 
 from .contract import LIVE_RUNTIME_SCHEMA_VERSION, build_so_live_runtime_snapshot
+from .ingest_coordinator import AwakeResolver, IngestPollResult, LiveCoreIngestCoordinator
 from .service import (
     BlueWolfRuntimeASGI,
     RuntimeSnapshotStore,
@@ -16,7 +17,10 @@ from .service import (
 
 __all__ = [
     "LIVE_RUNTIME_SCHEMA_VERSION",
+    "AwakeResolver",
     "BlueWolfRuntimeASGI",
+    "IngestPollResult",
+    "LiveCoreIngestCoordinator",
     "RuntimeSnapshotStore",
     "app",
     "build_so_live_runtime_snapshot",
