e8ed084ce369eb13424008e718830f0900e64317
roeemeir <rughroee2000@gmail.com>
2026-09-15 21:30:48 +0300

    Route template editing through governed direct-placement workbench

--- components/bluewolf/developer-governance-workbench.tsx
+++ components/bluewolf/developer-governance-workbench.tsx
@@ -4,16 +4,19 @@ import { useWorkspace } from "./app-context";
 import { DeveloperView } from "./developer-view";
 import { QaTruthWorkbench } from "./qa-truth-workbench";
 import { RouteBankWktWorkbench } from "./route-bank-wkt-workbench";
+import { TemplateGovernanceWorkbench } from "./template-governance-workbench";
 import { VehicleRangeWorkbench } from "./vehicle-range-workbench";
 
 export function DeveloperGovernanceWorkbench() {
   const { revision } = useWorkspace();
   return <>
+    <TemplateGovernanceWorkbench />
     <RouteBankWktWorkbench key={`route-bank-${revision}`} />
     <VehicleRangeWorkbench />
     <QaTruthWorkbench />
     <div className="protected-legacy-developer">
       <style>{`
+        .protected-legacy-developer .developer-nav nav > button:nth-child(2),
         .protected-legacy-developer .developer-nav nav > button:nth-child(3),
         .protected-legacy-developer .developer-nav nav > button:nth-child(6),
         .protected-legacy-developer .developer-nav .core-state {
